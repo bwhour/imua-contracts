a5fccf9257536db91073eb97b0c894998ac1f009
bwhour <juvemeen.xio@gmail.com>
2024-11-04 20:43:35 +0800

    update path

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 # Step 1: Define the contract name and output path
 CONTRACT_NAME="ExocoreBtcGateway"
-OUTPUT_PATH="/Users/will/go/src/github.com/ExocoreNetwork/bridge_private"
+OUTPUT_PATH="/Users/will/go/src/github.com/ExocoreNetwork/utxo-restaking"
 PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 CLIENT_CHAIN_ID=111
 RPC_URL="http://127.0.0.1:8546"
